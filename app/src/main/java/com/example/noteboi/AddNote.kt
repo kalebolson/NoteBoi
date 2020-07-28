018e3bc266a766da1f791b0a5d0cb46a7757aa17
Kaleb Olson <kalebroyolson@gmail.com>
2020-07-27 21:30:38 -0500

    Note Title updates on toolbar

--- app/src/main/java/com/example/noteboi/AddNote.kt
+++ app/src/main/java/com/example/noteboi/AddNote.kt
@@ -2,8 +2,13 @@ package com.example.noteboi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.widget.EditText
 import androidx.appcompat.widget.Toolbar
+import androidx.core.widget.addTextChangedListener
+import java.util.*
 
 class AddNote : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +16,37 @@ class AddNote : AppCompatActivity() {
         setContentView(R.layout.activity_add_note)
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+        supportActionBar?.title="New Note"
+
         val noteTitle = findViewById<EditText>(R.id.noteTitle)
         val noteDetails = findViewById<EditText>(R.id.noteDetails)
+
+        noteTitle.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s?.length != 0) {
+                    supportActionBar?.title = s
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                
+            }
+        })
+
+        val todaysDate = ""+(Calendar.MONTH+1)+"/"+Calendar.DAY_OF_MONTH+"/"+Calendar.YEAR
+        val currentTime = ""+pad(Calendar.HOUR)+":"+pad(Calendar.MINUTE)
+
+        Log.d("calendar", "Date: $todaysDate | Time: $currentTime")
+    }
+
+    private fun pad(i: Int) : String {
+        if(i<10){
+            return "0"+i
+        }
+        else return ""+i
     }
 }
